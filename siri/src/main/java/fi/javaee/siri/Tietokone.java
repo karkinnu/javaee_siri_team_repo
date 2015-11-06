3c25b4bb09d8c7f670a1dbf5fbcf7205a621bd71
jouni <jouni.hamalainen@kolumbus.fi>
2015-11-06 09:45:28 +0200

    201511060945

--- siri/src/main/java/fi/javaee/siri/Tietokone.java
+++ siri/src/main/java/fi/javaee/siri/Tietokone.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.*;
  
-// tähän tehty kommentti 2 kommitin kokeilua varten     jha 201511051315
+// tähän tehty kommentti 2 kommitin kokeilua varten     jha 201511060945
 /**
  * Entity JPA toteututs.
  * Managed Bean tarjotaan JSF:lle. VOidaan käyttää JSF:n kontrollerista
